f505535ab49936ebaecae0fa6be57363b44da577
qingge <qgkang@163.com>
2017-03-28 22:28:00 +0800

    leave word code

--- liuyan_ok.php
+++ liuyan_ok.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Qingge
- * Date: 2017/3/27
- * Time: 14:12
- */
+    header("Content-Type:text/html; charset=utf-8");
+    require("class/mysql_class.php");
+    //get ip
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $date = date("Y-m-d");
+    if($_POST['lytitle']!=null && $_POST['lycontent']!=null){
+        $insert = $db->insert("ve_hiu","title,content,data,ip","'".$_POST['lytitle']."','".$_POST['lycontent']."','$date','$ip'");
+        echo "<script>alert('留言成功！');window.location.href='index.php'</script>";
+    }else{
+        echo "<script>alert('请填写留言内容');window.location.href='liuyan.php';</script>";
+    }
